5fa00c3ffed6c8dd22aba4e403ee5fd149cff775
Oliver Nautsch <oliver@brixel.io>
2023-11-23 14:43:28 +0100

    v5 our cards

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,14 +2,45 @@ package org.leanpoker.player;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
-    static final String VERSION = "3 - pretty";
+public class Player {
+    static final String VERSION = "5 - our cards";
+
+    private List<Card> mycards = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "mycards=" + mycards +
+                '}';
+    }
+
 
     public static int betRequest(JsonNode request) {
+
+        Player we = new Player();
+
         System.out.println("request");
         System.out.println(request.toPrettyString());
 
+        request.get("players").forEach(player -> {
+            System.out.println("player");
+            System.out.println(player.toPrettyString());
+        });
+
+        JsonNode players = request.get("players");
+
+        players.forEach(player -> {
+            if ("Green Rabbits".equals(player.get("name").asText())) {
+                player.get("hole_cards").forEach(card -> {
+                    we.mycards.add(new Card(card.get("rank").asText(), card.get("suit").asText()));
+                });
+            }
+
+            System.out.println("found us: " + we);
+        });
 
         return 1000;
     }
