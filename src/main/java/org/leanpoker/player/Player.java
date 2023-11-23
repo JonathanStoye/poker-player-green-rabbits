21f4cea9e03ffc44b5f6a73eddd075bb29442dda
Oliver Nautsch <oliver@brixel.io>
2023-11-23 14:24:25 +0100

    v3

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,16 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class Player {
 
-    static final String VERSION = "2 - with sysout";
+    static final String VERSION = "3 - pretty";
 
     public static int betRequest(JsonNode request) {
         System.out.println("request");
-        System.out.println(request);
+        System.out.println(request.toPrettyString());
+
+
         return 1000;
     }
 
     public static void showdown(JsonNode game) {
         System.out.println("game at showdown");
-        System.out.println(game);
+        System.out.println(game.toPrettyString());
     }
 }
