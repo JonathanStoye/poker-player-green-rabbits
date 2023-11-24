e27a827c660c74cd2829ee8a0561b92b48783d1e
Oliver Nautsch <oliver@brixel.io>
2023-11-24 16:03:37 +0100

    v22

--- src/main/java/org/leanpoker/player/BetService.java
+++ src/main/java/org/leanpoker/player/BetService.java
@@ -1,15 +1,10 @@
 package org.leanpoker.player;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.leanpoker.player.raw.GameStateRaw;
-
-import static org.leanpoker.player.Rank.N_10;
 
 public class BetService {
     private final int bet;
-    static final String VERSION = "v21 - strat";
+    static final String VERSION = "v22 - back to 13";
 
     public BetService(int bet) {
         this.bet = bet;
@@ -17,9 +12,6 @@ public class BetService {
 
 
     public static int betRequest(JsonNode request) {
-        GameStateRaw gameStateRaw = getGameStateRaw(request);
-        GameState gameState = new GameState(gameStateRaw);
-
         BetService we = null;
         AllCards allCards = new AllCards();
 
@@ -32,14 +24,6 @@ public class BetService {
         });
 
         JsonNode players = request.get("players");
-
-        gameState.getUs().getHoleCards().forEach(card -> {
-            allCards.addMyCard(
-                    new Card(
-                            Rank.getRank(card.getRank()),
-                            Suit.getSuit(card.getSuit())));
-        });
-
 
         // iterate the players and find our player
         for (JsonNode player : players) {
@@ -55,53 +39,34 @@ public class BetService {
             }
         }
 
-        gameStateRaw.getCommunityCards().forEach(cardRaw -> {
+        request.get("community_cards").forEach(card -> {
             allCards.addCommunityCard(
-                    new Card(cardRaw)
+                    new Card(
+                            Rank.getRank(card.get("rank").asText()),
+                            Suit.getSuit(card.get("suit").asText()))
             );
         });
 
+        int mimimumRaise = request.get("minimum_raise").asInt();
+        int currentBuyIn = request.get("current_buy_in").asInt();
 
         if (allCards.hasEqualCardsWithMinWeightAndMinNumber(1, 4)) {
-            System.out.println("allCards.hasEqualCardsWithMinWeightAndMinNumber(1, 4)");
-            return 1000 + gameStateRaw.getCurrentBuyIn();
+            return 1000 + currentBuyIn;
         }
 
         if (allCards.hasEqualCardsWithMinWeightAndMinNumber(1, 3)) {
-            System.out.println("allCards.hasEqualCardsWithMinWeightAndMinNumber(1, 3)");
-            return 500 + gameStateRaw.getCurrentBuyIn();
+            return 500 + currentBuyIn;
         }
 
-
-        if (allCards.hasEqualCardsWithMinWeightAndMinNumber(10, 2) ) {
-            System.out.println("allCards.hasEqualCardsWithMinWeightAndMinNumber(10, 2)");
-            return gameStateRaw.getCurrentBuyIn() - we.bet + gameStateRaw.getMinimumRaise() + gameStateRaw.getMinimumRaise();
+        if (allCards.hasEqualCardsWithMinWeightAndMinNumber(10, 2)) {
+            return currentBuyIn - we.bet + mimimumRaise;
         }
 
-        if (allCards.isWithoutCommunityCards() && allCards.hasMinRank(N_10)) {
-            System.out.println("allCards.isWithoutCommunityCards() && allCards.hasMinRank(N_10)");
-            return gameStateRaw.getCurrentBuyIn() - we.bet + gameStateRaw.getMinimumRaise();
-        }
-
-        if (gameStateRaw.getCurrentBuyIn() < 10 ) {
-            System.out.println("gameStateRaw.getCurrentBuyIn() < 10");
-            return gameStateRaw.getCurrentBuyIn() - we.bet; // calling
+        if (currentBuyIn < 10) {
+            return currentBuyIn - we.bet; // calling
         }
 
         return 0;
-    }
-
-    private static GameStateRaw getGameStateRaw(JsonNode request) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            GameStateRaw gameStateRaw = objectMapper.treeToValue(request, GameStateRaw.class);
-            System.out.println(gameStateRaw.toString());
-            return gameStateRaw;
-        } catch (JsonProcessingException e) {
-            System.out.println("error parsing json");
-            System.out.println(request.toPrettyString());
-        }
-        return null;
     }
 
     public static void showdown(JsonNode game) {
